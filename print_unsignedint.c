2d74909791a03bd9f3f8d71b0bf145f6305714d1
mai <mai0627.my@gmail.com>
2023-10-13 13:48:32 +0900

    auto push from mai_laptop

--- print_unsignedint.c
+++ print_unsignedint.c
@@ -6,26 +6,21 @@
 /*   By: myokono <myokono@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 16:39:15 by myokono           #+#    #+#             */
-/*   Updated: 2023/10/13 11:13:07 by myokono          ###   ########.fr       */
+/*   Updated: 2023/10/13 13:47:27 by myokono          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_putnbr_unsigned(unsigned int n)
+int	ft_print_unsignedint(unsigned int n)
 {
-	char	c;
+    char	c;
 	int		len;
 
 	len = 0;
 	if (n >= 10)
-		len += ft_putnbr_unsigned(n / 10);
+		len += ft_print_unsignedint(n / 10);
 	c = (n % 10) + '0';
 	len += ft_print_char(c);
 	return (len);
-}
-
-int	ft_print_unsignedint(unsigned int n)
-{
-	return (ft_putnbr_unsigned(n));
 }
