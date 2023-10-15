167f7e2b4f504dcafb168c4562659dbe54dbf4ba
mai <mai0627.my@gmail.com>
2023-10-15 19:41:19 +0900

    auto push from mai_laptop

--- ft_printf.h
+++ ft_printf.h
@@ -6,16 +6,16 @@
 /*   By: myokono <myokono@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 15:28:33 by myokono           #+#    #+#             */
-/*   Updated: 2023/10/15 13:47:01 by myokono          ###   ########.fr       */
+/*   Updated: 2023/10/15 19:15:35 by myokono          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "libft/libft.h"
 # include <stdarg.h>
 # include <unistd.h>
+# include <limits.h>
 
 ssize_t	ft_branch(const char c, va_list args);
 
